712fd64ec3ed8beda6096b7d415d502d6fc9ccac
Myself <tanminecraft2@gmail.com>
2024-12-28 22:40:25 +0700

    This one is correct.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,7 +46,6 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    dbg_hit_on(std::abs(simpleEval) > 962);
     return std::abs(simpleEval) > 962;
 }
 
