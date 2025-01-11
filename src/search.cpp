f8ee8c423a986123518778ae0ef6eb4e2bb2b55e
Myself <tanminecraft2@gmail.com>
2025-01-12 00:08:42 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1354,7 +1354,7 @@ moves_loop:  // When in check, search starts here
         // remember it, to update its stats later.
 
         //Update: only if value is not much worse.
-        if (move != bestMove && moveCount <= 32 && (value + 1200 - (200*capture) >= bestValue))
+        if (move != bestMove && moveCount <= 32 && (value + 1400 - (400*capture) >= bestValue))
         {
             if (capture)
                 capturesSearched.push_back(move);
