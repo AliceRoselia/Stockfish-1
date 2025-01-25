ee91b9d963ddeb7b498e68fcf08de6cabcc1ba14
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-01-26 06:55:05 +0700

    Fix bug.

--- src/tt.cpp
+++ src/tt.cpp
@@ -142,7 +142,7 @@ void TTWriter::write(
 
     Cluster* cluster = reinterpret_cast<Cluster*>(entry&(~3));
     uintptr_t loc = entry&3;
-    TTEntry* entry2 = &((cluster->entry)[loc]);
+    TTEntry* entry2 = &((cluster->keys)[loc]);
 
     entry2->save(&(cluster->entry),loc,k, v, pv, b, d, m, ev, generation8);
 }
