853c6425f828837fc1727fe845a2fe9b2b965b18
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-02-01 17:43:44 +0700

    Update history.h

--- src/history.h
+++ src/history.h
@@ -87,7 +87,7 @@ class StatsEntry {
         // Make sure that bonus is in range [-D, D]
         int clampedBonus = std::clamp(bonus, -D, D);
         int squaredBonus = clampedBonus * clampedBonus;
-        entry += clampedBonus - (entry * std::abs(clampedBonus)  - squaredBonus)/ D + squaredBonus*entry/(D*D);
+        entry += clampedBonus - (entry * std::abs(clampedBonus)  - squaredBonus/2)/ D + squaredBonus*entry/(2*D*D);
 
         assert(std::abs(entry) <= D);
     }
