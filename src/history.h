f786efd1d78a27a8aeb5805e7026606bb02d76b6
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-02-04 04:55:24 +0700

    Queen value = 11.

--- src/history.h
+++ src/history.h
@@ -64,10 +64,10 @@ inline int non_pawn_index(const Position& pos) {
 }
 
 inline int material_imbalance(const Position& pos, Color side){
-    int material = (pos.count<PAWN>(side) + pos.count<KNIGHT>(side)*3 + pos.count<BISHOP>(side)*4 + pos.count<ROOK>(side)*6 + pos.count<QUEEN>(side)*12 -
-                    pos.count<PAWN>(~side) + pos.count<KNIGHT>(~side)*3 + pos.count<BISHOP>(~side)*4 + pos.count<ROOK>(~side)*6 + pos.count<QUEEN>(~side)*12
+    int material = (pos.count<PAWN>(side) + pos.count<KNIGHT>(side)*3 + pos.count<BISHOP>(side)*4 + pos.count<ROOK>(side)*6 + pos.count<QUEEN>(side)*11 -
+                    pos.count<PAWN>(~side) + pos.count<KNIGHT>(~side)*3 + pos.count<BISHOP>(~side)*4 + pos.count<ROOK>(~side)*6 + pos.count<QUEEN>(~side)*11
     );
-    return std::clamp(material,-4,4)+4;
+    return std::clamp(material,-6,6)+6;
 }
 
 // StatsEntry is the container of various numerical statistics. We use a class
@@ -119,7 +119,7 @@ using LowPlyHistory =
   Stats<std::int16_t, 7183, LOW_PLY_HISTORY_SIZE, int(SQUARE_NB) * int(SQUARE_NB)>;
 
 // CapturePieceToHistory is addressed by a move's [material_imbalance][piece][to][captured piece type]
-using CapturePieceToHistory = Stats<std::int16_t, 10692,9, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using CapturePieceToHistory = Stats<std::int16_t, 10692,13, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 // PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 using PieceToHistory = Stats<std::int16_t, 30000, PIECE_NB, SQUARE_NB>;
