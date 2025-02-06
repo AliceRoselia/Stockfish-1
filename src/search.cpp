91f702e69258be24676ac2e1e4f6c5ed6bd04656
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-02-06 08:56:34 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -939,8 +939,8 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 413;
-    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
+    probCutBeta = beta + std::max((depth-ttData.depth)*104, 104);
+    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 8 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
 
