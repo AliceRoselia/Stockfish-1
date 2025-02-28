716a68201d6b81030dfa552987066881d55e63e6
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-02-28 12:15:53 +0700

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -187,7 +187,8 @@ void MovePicker::score() {
         else  // Type == EVASIONS
         {
             if (pos.capture_stage(m))
-                m.value = PieceValue[pos.piece_on(m.to_sq())] + (1 << 28);
+                m.value = 7 * int(PieceValue[pos.piece_on(m.to_sq())])
+              + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))] + (1 << 28);
             else
                 m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
                         + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
