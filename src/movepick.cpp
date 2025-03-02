60b7d96787172c20761de874c5dfd888cbafb634
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-03-02 20:58:07 +0700

    Take 3

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -204,7 +204,7 @@ void MovePicker::score() {
                 int threats_on_square = pos.threats_on_square(m);
                 int threats_from_square = pos.threats_from_square(m);
                 m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
-                        + (*threatHistory)[pos.moved_piece(m)][m.to_sq()][std::min(threats_from_square,2)][std::clamp(threats_on_square,-2,2)+2]/2;
+                        + (*threatHistory)[pos.moved_piece(m)][m.to_sq()][std::min(threats_from_square,2)][std::clamp(threats_on_square,-2,2)+2]/2
                         + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
                         + (*pawnHistory)[pawn_structure_index(pos)][pos.moved_piece(m)][m.to_sq()];
             }
