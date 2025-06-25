af51f4c2a96ecab60fd2d55edaa00221fdea3dae
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-06-25 16:40:29 +0700

    Fixed.
    bench: 2372941

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -164,7 +164,7 @@ void MovePicker::score() {
             m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
 
             m.value += (pt == KNIGHT && more_than_one(attacks_bb<KNIGHT>(to)& (pos.pieces(~us,ROOK,KING,QUEEN)))) * 3000;
-            m.value += (pt == BISHOP && more_than_one(attacks_bb<BISHOP>(to)& (pos.pieces(~us,ROOK,KING,QUEEN)))) * 3000;
+            m.value += (pt == BISHOP && more_than_one(attacks_bb<BISHOP>(to,pos.pieces())& (pos.pieces(~us,ROOK,KING,QUEEN)))) * 3000;
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
             if (KNIGHT <= pt && pt <= QUEEN)
