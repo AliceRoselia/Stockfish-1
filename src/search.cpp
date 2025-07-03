4235e4854aa9901fbffd84b75cc9d6bc268e2668
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-07-03 22:24:08 +0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -63,7 +63,7 @@ using namespace Search;
 
 namespace {
 
-constexpr int SEARCHEDLIST_CAPACITY = 16;
+constexpr int SEARCHEDLIST_CAPACITY = 8;
 using SearchedList                  = ValueList<Move, SEARCHEDLIST_CAPACITY>;
 
 // (*Scalers):
