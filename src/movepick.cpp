804231463dca7970e17ab40e59be1cee275d14fc
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2025-09-29 20:07:23 +0700

    Update movepick.cpp
    bench: 1938063

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -156,6 +156,8 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
         threatByLesser[QUEEN] = pos.attacks_by<ROOK>(~us) | threatByLesser[ROOK];
         threatByLesser[KING]  = pos.attacks_by<QUEEN>(~us) | threatByLesser[QUEEN];
     }
+    [[maybe_unused]] Square prevsq = SQ_NONE;
+    [[maybe_unused]] int threatValue = 0;
 
     ExtMove* it = cur;
     for (auto move : ml)
@@ -187,17 +189,22 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             // bonus for checks
             m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
 
+
+
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
             static constexpr int bonus[KING + 1] = {0, 0, 144, 144, 256, 517, 10000};
             int v = threatByLesser[pt] & to ? -95 : 100 * bool(threatByLesser[pt] & from);
             m.value += bonus[pt] * v;
 
-            m.value += threatByLesser[pt] & from ? threat_to(from,pt)*3 : 0;
+            if (prevsq != from)
+                threatValue = threatByLesser[pt] & from ? threat_to(from,pt)*3 : 0;
+            m.value += threatValue;
 
 
             if (ply < LOW_PLY_HISTORY_SIZE)
                 m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
+            prevsq = from;
         }
 
         else  // Type == EVASIONS
