5d74ccd095bcfbcc8891c65a439b9f0045a7f7a0
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-11-15 12:56:09 +0700

    Update search.cpp
    bench: 3031329

--- src/search.cpp
+++ src/search.cpp
@@ -827,7 +827,7 @@ Value Search::Worker::search(
 
         if (priorCapture)
         {
-            captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())] << evalDiff * 6;
+            captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())] << evalDiff * 12;
         }
         else
         {
