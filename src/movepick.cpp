e1de8ba3527b3c04326fcdb226843cb7d72d4ab2
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-11-27 16:54:53 +0700

    Update movepick.cpp
    bench: 2595542

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -160,18 +160,18 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
         {
             // histories
             //These were tuned.
-            m.value =  3426 * (*mainHistory)[us][m.raw()];
-            m.value += 2451 * (*pawnHistory)[pawn_history_index(pos)][pc][to];
-            m.value += 1786 * (*continuationHistory[0])[pc][to];
-            m.value += 1004 * (*continuationHistory[1])[pc][to];
-            m.value += 841 * (*continuationHistory[2])[pc][to];
-            m.value += 984 * (*continuationHistory[3])[pc][to];
-            m.value += 552 * (*continuationHistory[5])[pc][to];
-            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 27264550;
-            m.value += threatByLesser[pt] & to ? -25663773 : 27078396 * bool(threatByLesser[pt] & from);
+            m.value =  3355 * (*mainHistory)[us][m.raw()];
+            m.value += 2452 * (*pawnHistory)[pawn_history_index(pos)][pc][to];
+            m.value += 2007 * (*continuationHistory[0])[pc][to];
+            m.value += 1005 * (*continuationHistory[1])[pc][to];
+            m.value += 945 * (*continuationHistory[2])[pc][to];
+            m.value += 1187 * (*continuationHistory[3])[pc][to];
+            m.value += 719 * (*continuationHistory[5])[pc][to];
+            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 27587670;
+            m.value += threatByLesser[pt] & to ? -25347700 : 27904235 * bool(threatByLesser[pt] & from);
 
             if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 6113 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
+                m.value += 6130 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
             //m.value /= 1024;
 
 
