1c7edd87e2257d1cee57da48b7f8d7a26624a4ef
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-12-10 18:45:15 +0700

    Update search.cpp
    bench: 2595235

--- src/search.cpp
+++ src/search.cpp
@@ -1146,7 +1146,7 @@ moves_loop:  // When in check, search starts here
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin) + (ss-1)->singular;
-
+                ss->singular = true;
                 depth++;
             }
 
