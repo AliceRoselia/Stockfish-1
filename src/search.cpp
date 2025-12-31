9b0673213cf5d813c8482c519c1e44e5bdfc0ece
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-12-31 15:12:37 +0700

    Update search.cpp
    bench: 2153652

--- src/search.cpp
+++ src/search.cpp
@@ -1418,7 +1418,7 @@ moves_loop:  // When in check, search starts here
     // Bonus for prior quiet countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonusScale = -215;
+        int bonusScale = -250;
         bonusScale -= (ss - 1)->statScore / 100;
         bonusScale += std::min(56 * depth, 489);
         bonusScale += 184 * ((ss - 1)->moveCount > 8);
