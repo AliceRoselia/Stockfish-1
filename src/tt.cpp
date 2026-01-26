a3f2459dea5bd6c58b6bd052052b1b65f6d51b8f
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2026-01-26 14:26:36 +0700

    Update tt.cpp
    bench: 2463811

--- src/tt.cpp
+++ src/tt.cpp
@@ -236,8 +236,8 @@ std::tuple<bool, TTData, TTWriter> TranspositionTable::probe(const Key key) cons
     // Find an entry to be replaced according to the replacement strategy
     TTEntry* replace = tte;
     for (int i = 1; i < ClusterSize; ++i)
-        if (replace->depth8 - replace->relative_age(generation8)
-            > tte[i].depth8 - tte[i].relative_age(generation8))
+        if (replace->depth8 - replace->relative_age(generation8) + (std::abs(replace->value16-replace->eval16) >= 418)
+            > tte[i].depth8 - tte[i].relative_age(generation8) + (std::abs(tte[i].value16-tte[i].eval16) >= 418))
             replace = &tte[i];
 
     return {false,
