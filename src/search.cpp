9df1c37d552f3c29fa8a13918768139e97b8e2e9
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-02-17 22:19:18 +0700

    Update search.cpp
    bench: 2442532

--- src/search.cpp
+++ src/search.cpp
@@ -1897,12 +1897,12 @@ void update_quiet_histories(
     workerThread.mainHistory[us][move.raw()] << mainBonus;  // Untuned to prevent duplicate effort
 
     if (ss->ply < LOW_PLY_HISTORY_SIZE)
-        workerThread.lowPlyHistory[ss->ply][move.raw()] << bonus * 805 / 1024;
+        workerThread.lowPlyHistory[ss->ply][move.raw()] << mainBonus * 805 / 1024;
 
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(), bonus * 896 / 1024);
 
     workerThread.sharedHistory.pawn_entry(pos)[pos.moved_piece(move)][move.to_sq()]
-      << mainBonus * (bonus > 0 ? 905 : 505) / 1024;
+      << bonus * (bonus > 0 ? 905 : 505) / 1024;
 }
 
 }
