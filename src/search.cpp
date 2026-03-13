5e8dd4e5436a6ea31f83af0d944693058c213e61
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-03-13 22:43:13 +0700

    Update search.cpp
    bench: 2554958

--- src/search.cpp
+++ src/search.cpp
@@ -890,7 +890,7 @@ Value Search::Worker::search(
     }
 
     // Step 9. Null move search with verification search
-    if (cutNode && ss->staticEval + 300 * (ttData.move && pos.gives_check(ttData.move)) >= beta - 17 * depth - 50 * improving + 359 && !excludedMove
+    if (cutNode && ss->staticEval - 300 * (ttData.move && pos.gives_check(ttData.move)) >= beta - 17 * depth - 50 * improving + 359 && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
     {
         assert((ss - 1)->currentMove != Move::null());
