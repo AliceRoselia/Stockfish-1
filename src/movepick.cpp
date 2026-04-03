6b788f2b3d5fd2c713e62459a4c0d920cb9056ca
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-04-03 17:49:57 +0700

    Update movepick.cpp
    bench: 2959021

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -171,7 +171,7 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
             // bonus for checks
             m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
 
-            m.value -= (*counterMoveGini)[pc][to]/8;
+            m.value -= (*counterMoveGini)[pc][to]/16;
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
