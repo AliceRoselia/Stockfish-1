033b2be76a6da135955a1e781164b84c9c7d2cd6
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-05-03 21:51:51 +0700

    Update search.cpp
    bench: 2795389

--- src/search.cpp
+++ src/search.cpp
@@ -1340,7 +1340,7 @@ moves_loop:  // When in check, search starts here
             int counterMoveSecondContHist = (*contHist[0])[counterMovedPiece][counterMovedSquare];
 
 
-            nextPlyGenericCounterMove = (counterMoveMainHist*2 + counterMoveSecondContHist > 8000);
+            nextPlyGenericCounterMove = (counterMoveMainHist*2 + counterMoveSecondContHist > 4000);
         }
 
         // Step 19. Undo move
